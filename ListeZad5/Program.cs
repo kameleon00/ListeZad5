e1ab826c8805b57c93ad2c5d2b2854670e98382f
Marko Djokic <marko1319@its.edu.rs>
2020-12-30 02:05:52 +0100

    Add project files.

---
+++ ListeZad5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<TKey, TValue>
+
+            Dictionary<string, int> gradovi = new Dictionary<string, int>()
+            {
+                {"Beograd", 2000000 },
+                {"Nis", 1400000 },
+                {"Kraljevo", 400000 },
+            };
+            Console.WriteLine("Grad - Stanovnici");
+            foreach (KeyValuePair<string, int> grad in gradovi)
+                Console.WriteLine(grad.Key + " - " + grad.Value);
+            Console.WriteLine("=====================================");
+
+            Console.WriteLine("Preko var-a");
+            foreach (var grad in gradovi)
+                Console.WriteLine(grad);
+            Console.WriteLine("=====================================");
+
+            Console.WriteLine("Samo vrednosti");
+            foreach (var grad in gradovi.Values)
+                Console.WriteLine(grad);
+            Console.WriteLine("=====================================");
+
+            Console.WriteLine("Samo kljucevi");
+            foreach (var grad in gradovi.Keys)
+                Console.WriteLine(grad);
+            Console.WriteLine("=====================================");
+
+            Console.ReadLine();
+        }
+    }
+}
